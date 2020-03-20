e17d980df071a865217e51eac809214d2712c441
lanoixdesign <lanoixdesign@gmail.com>
2020-03-20 23:31:27 +0100

    first commit

---
+++ asset/js/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+
+ $("#rond2").click(function(event){
+        // $(".menuRond").addClass('ok').removeClass('ko');
+        
+   })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    });
---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="fr_FR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>MonÉgliseCeev.net</title>
+
+    <meta name="author" description="Micaël Lanoix">
+    <meta name="description" content="">
+
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"
+    integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+
+    <link rel="stylesheet" href="asset/css/style.css">
+    <script src="asset/js/script.js"></script>
+
+</head>
+<body>
+
+<header class="" >
+
+    <!-- <div class="col-12 p-3">
+    <ul class="nav justify-content-center">
+        <li class="nav-item">
+          <a class="nav-link active" href="#">Live</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Agenda</a>
+          </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Dons & Offrandes</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Contacts</a>
+          </li>
+      </ul>
+    </div> -->
+
+
+    <div id="capsule" class="d-flex align-items-center justify-content-center ">
+
+            <div id="rond" class=" shadow">
+                <div id="rond2" class="zoomer shadow">
+                    <!-- <div id="rond2" class="zoomer2 shadow img-fluid"></div> -->
+                </div>
+            </div>
+    </div>
+
+
+</header>
+
+<main>
+
+
+    <?php include('main.php') ?>
+
+
+</main>
+
+<footer></footer>
+</body>
+
+
+
+</html>
+
+

0bef63bb036c66671180560253bf7be8fbd090fd
lanoixdesign <lanoixdesign@gmail.com>
2020-03-22 07:47:26 +0100

    changement d'index

--- index.php
+++ index.php
@@ -1,47 +1,95 @@
-<?php include_once('common/_header.php');?>
+<!DOCTYPE html>
+<html lang="fr_FR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>MonÉgliseCeev.net</title>
 
-    <div id="grid">
+    <meta name="author" description="Micaël Lanoix">
+    <meta name="description" content="">
+
+    <script src="/yts/jsbin/webcomponents-sd-vflBmTtv7/webcomponents-sd.js" type="text/javascript" name="webcomponents-sd/webcomponents-sd"></script>
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"
+    integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
 
 
-        <div class="m-left">
-            <ul>
-                <li>
-                    <div><img src="asset/media/vacation.png" alt="agenda ceev"></div>
-                    <h5>Agenda</h5>
-                </li>
-                <li>
-                    <div><img src="asset/media/live-chat.png" alt="live ceev"></div>
-                    <h5>Tchat & live</h5>
-                </li>
-                <li>
-<div><img src="asset/media/information.png" alt="Ceev Infos"></div>
-                    <h5>Infos</h5>
-                </li>
+    <link rel="stylesheet" href="asset/css/style.css">
+    <script src="asset/js/script.js"></script>
 
-            </ul>
+</head>
+<body>
+
+<header>
+
+<iframe id="vid_frame" width="100%" height="288"  src="https://www.youtube.com/embed/live_stream?channel=UCt34B9T0UAI-Q-uocpb37yA" frameborder="0" gesture="media" allowfullscreen></iframe>
+
+</header>
+<main>
+    <section>
+        <div class="urgent">
+            <div>
+                <h4>Bienvenue sur MonÉgliseCeev.net</h4>
+                <p><strong><span class="warning">À la une : </span></strong>Les églises en famille continues...</p>
+            <button class="btn">En savoir plus</button>
+            </div>
+            
+            <div id="burger">
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div>
+
+        </div>
+        
+    </section>
+
+    <section>
+        <div id="supermenu">
+            <?php require('common/_menu.php');?>
+        </div>
+        <div id="actu" class="ko">
+            <?php include_once('common/_actu.php');?>
+        </div>
+        <div id="agenda" class="ko">
+            <?php include_once('common/_agenda.php');?>
+        </div>
+
+        <div id="live" class="ko">
+            <?php include_once('common/_live.php');?>
+        </div>
+
+        <div id="info" class="ko">
+            <?php include_once('common/_info.php');?>
+        </div>
+
+        <div id="don" class="ko">
+            <?php include_once('common/_don.php');?>
+        </div>
+
+        <div id="chant" class="ko">
+            <?php include_once('common/_chant.php');?>
+        </div>
+
+        <div id="priere" class="ko">
+            <?php include_once('common/_priere.php');?>
         </div>
 
 
-        <div class="m-right">     
-            <ul>
-                <li>
-                    <div><img src="asset/media/credit-card.png" alt="paiement sécurisé"></div>
-                    <h5>Dîmes & Offrandes</h5>
-                </li>
-                <li>
-                    <div><img src="asset/media/music-sheet.png" alt="Paroles des chants"></div>
-                    <h5>Paroles des Chants</h5>
-                </li>
-                <li>
-                    <div><img src="asset/media/cross.png" alt="Sujets de prière"></div>
-                    <h5>Vos sujets de Prière</h5>
-                </li>
-
-            </ul>
-        </div>
 
 
-    </div>
 
 
-    <?php include_once('common/_footer.php');?>
+
+    </section>    
+
+
+
+</main>
+<footer></footer>
+
+</body>
+</html>
